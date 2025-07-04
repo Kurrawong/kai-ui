633576b2efe2de820a54ca078219dfe660ae356b
jamiefeiss <jamie.feiss@gmail.com>
2025-07-04 16:51:55 +1000

    Removed map from index.ts for now, fixed Editor container queries priority order

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import "./assets/index.css";
 
 import { Editor } from "./components/editor";
-import Map from "./components/map/Map.vue";
+// import Map from "./components/map/Map.vue";
 
 export * from "./types";
 
 export {
     Editor,
-    Map,
+    // Map,
 };
